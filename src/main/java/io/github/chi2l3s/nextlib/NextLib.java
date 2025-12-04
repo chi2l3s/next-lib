e86b4be33eee77e280aff05d25db6d188f9f70ff
chi2l3s <amixolplex@gmail.com>
2025-12-04 16:55:07 +0700

    Update 1.0.5.1

--- src/main/java/io/github/chi2l3s/nextlib/NextLib.java
+++ src/main/java/io/github/chi2l3s/nextlib/NextLib.java
@@ -2,8 +2,7 @@ package io.github.chi2l3s.nextlib;
 
 import io.github.chi2l3s.nextlib.api.color.ColorUtil;
 import io.github.chi2l3s.nextlib.api.color.ColorUtilImpl;
-import io.github.chi2l3s.nextlib.internal.Metrics;
-import io.github.chi2l3s.nextlib.internal.util.UpdateChecker;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NextLib extends JavaPlugin {
@@ -12,9 +11,7 @@ public final class NextLib extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        new UpdateChecker(this, "chi2l3s/next-lib").checkForUpdates();
-        int pluginId = 27705;
-        new Metrics(this, pluginId);
+        Bukkit.getLogger().info("[ NextLib ] Enabled");
     }
 
     @Override
